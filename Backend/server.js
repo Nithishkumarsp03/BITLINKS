e9a8f9367b8f51236d58b42bca473a8871a37a6f
Nithishkumarsp03 <nithishkumar3115@gmail.com>
2024-08-06 18:00:06 +0530

    Add new folder and files

---
+++ Backend/server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const multer = require("multer");
+const path = require("path");
+const app = express();
+const cors = require("cors");
+const PORT = process.env.PORT || 8000;
+
+
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "bitlinks",
+  connectionLimit: 200,
+});
+
+
+app.post("/api/google", (req, res) => {
+    const { name } = req.body;
+  
+    pool.getConnection((err, connection) => {
+      if (err) {
+        console.error("Error getting database connection:", err);
+        return res.status(500).json({ message: "Database error" });
+      }
+  
+      connection.query(
+        "SELECT * FROM login WHERE NAME = ?",
+        [name],
+        (err, results) => {
+          connection.release();
+  
+          if (err) {
+            console.error("Error executing database query:", err);
+            return res.status(500).json({ message: "Database error" });
+          }
+  
+          if (results.length > 0) {
+            res
+              .status(200)
+              .json({ message: "Login successful" });
+          } else {
+            res.status(401).json({ message: "Invalid username or password" });
+          }
+        }
+      );
+    });
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
+import Home from './PAGES/Home/Home';
+import Login from './PAGES/Login/Login';
+import Flowchart from './COMPONENTS/flowchart';
+import MainFlow from './PAGES/Flows/MainFlow/Flows';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+        <Routes>
+          <Route path='/' element={<Login/>} />
+          <Route path='/dashboard' element={<Home/>} />
+          <Route path='/flowchart' element={<MainFlow />} />
+        </Routes>
+    </>
   );
 }
 
